f9ec75f59abb7ea6bdeaee4b96cc5cb68683b242
Marian Oancea <contact@siteshop.ro>
2014-07-15 06:04:51 +0300

    Added Facades

---
+++ src/Siteshop/Html/Facades/Form.php
@@ -0,0 +1,14 @@
+<?php namespace Illuminate\Support\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Form extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'form'; }
+
+}
---
+++ src/Siteshop/Html/Facades/HTML.php
@@ -0,0 +1,14 @@
+<?php namespace Siteshop\Html\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class HTML extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'html'; }
+
+}
