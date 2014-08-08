bff313a7b0f29a829f6753e76175327871426217
Marian Oancea <contact@siteshop.ro>
2014-08-08 21:35:29 +0300

    Added imageLink to HTMLBuilder

--- src/Siteshop/Html/HtmlBuilder.php
+++ src/Siteshop/Html/HtmlBuilder.php
@@ -18,4 +18,23 @@ class HtmlBuilder extends \Illuminate\Html\HtmlBuilder
 
 		return preg_replace_callback('#\{i\.([^}]+)\}#iU', $callback, $input);
 	}
+
+	/**
+	 *   Generate image link
+	 *
+	 *   @param     string    $link_url         Link URL
+	 *   @param     string    $image_url        Image URL
+	 *   @param     string    $title            Image alt
+	 *   @param     array     $link_options     Link attributes
+	 *   @param     array     $image_options    Image attributes
+	 *
+	 *   @return    string
+	 */
+	public function imageLink($link_url, $image_url, $title, $link_options = [], $image_options = [], $secure)
+	{
+		$link = HTML::link($link_url, '{image}', $options['image'], $secure);
+		$image = HTML::image($image_url, $title, $image_options);
+
+		return str_replace('{image}', $image, $link);
+	}
 }
